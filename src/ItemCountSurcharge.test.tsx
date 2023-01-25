cd7ee4a22f176dd3bf486139e81352b624df03fa
Duyen-codes <hongduyen0705@gmail.com>
2023-01-25 10:44:10 +0200

    write tests for ItemCountSurcharge

---
+++ src/ItemCountSurcharge.test.tsx
@@ -0,0 +1,22 @@
+import ItemCountSurcharge from './ItemCountSurcharge'
+
+describe('ItemCountSurcharge', () => {
+  let surcharge: any
+
+  beforeEach(() => {
+    surcharge = new ItemCountSurcharge(5, 10) // itemCount, fee
+  })
+  test('calculate() should add 0.5 for each item over 4', () => {
+    expect(surcharge.calculate()).toBe(10.5)
+  })
+
+  test('calculate() should add 0.5 for each item over 4 + 1.2 if itemCount is over 12', () => {
+    surcharge.itemCount = 13
+    expect(surcharge.calculate()).toBe(15.7)
+  })
+
+  test('should not add surcharge for itemCount less than 5', () => {
+    surcharge.itemCount = 4
+    expect(surcharge.calculate()).toBe(10)
+  })
+})
